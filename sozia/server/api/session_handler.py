fa8787d4f8723643c259bdcca41be2e0f1ca1dfd
Aylin Barutcu <a.barutcu@etu.edu.tr>
2026-04-19 20:36:32 +0300

    tune(api): reduce watchdog interval to 500 ms for snappier utterance detection

--- sozia/server/api/session_handler.py
+++ sozia/server/api/session_handler.py
@@ -100,13 +100,13 @@ class SessionHandler:
         silence is detected even if the client pauses audio delivery.
         """
         while True:
-            await asyncio.sleep(1.0)
+            await asyncio.sleep(0.5)
             if self.modality_path != ModalityPath.SPEECH:
                 continue
             try:
                 await self._orchestrator.flush_stale_speech(
                     self.session_id,
-                    1.0,
+                    0.5,
                     list(self.latest_health.values()),
                     self.send_segment,
                 )
